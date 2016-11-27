db01bf15570248e2708016c474b736b6acf0f049
Raseswari Das <raseswaridas@Raseswaris-MacBook-Pro.local>
2016-11-27 16:50:19 -0500

    Added all the POJO classes

---
+++ Runtime Environment/src/business/person/Person.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.person;
+
+/**
+ *
+ * @author raseswaridas
+ */
+public class Person {
+
+    private String name;
+    private String contact;
+    private String address;
+    private int age;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
