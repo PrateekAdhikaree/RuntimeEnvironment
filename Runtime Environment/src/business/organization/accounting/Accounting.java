4bf8681747173442932f9f9616a989f397062340
Raseswari Das <raseswaridas@Raseswaris-MacBook-Pro.local>
2016-12-06 03:39:59 -0500

    Added code to calculate revenue monthly and for any number of months

--- Runtime Environment/src/business/organization/accounting/Accounting.java
+++ Runtime Environment/src/business/organization/accounting/Accounting.java
@@ -28,6 +28,10 @@ public class Accounting extends Organization{
     public int getCurrentFunds() {
         return currentFunds;
     }
+    
+    public void setCurrentFunds(int value){
+        currentFunds = value;
+    }
 
     public MembershipDirectory getMembershipDirectory() {
         return membershipDirectory;
@@ -37,7 +41,7 @@ public class Accounting extends Organization{
         this.membershipDirectory = membershipDirectory;
     }
 
-    public void setCurrentFunds(EmployeeDirectory employeeDirectory, CustomerDirectory customerDirectory) {
+    public void calculateRevenue(EmployeeDirectory employeeDirectory, CustomerDirectory customerDirectory, int months) {
         int totalFunds = 0;
         int totalEmployeeSalary = 0;
         int totalCustomerFees = 0;
@@ -49,13 +53,17 @@ public class Accounting extends Organization{
             totalCustomerFees += calculateCustomerFees(customer);
         }
         
+        //Calculates revenue for 1 month
         totalFunds = totalCustomerFees - totalEmployeeSalary;
+        //Calculates revenue for required months
+        totalFunds = totalFunds * months;
         this.currentFunds = totalFunds;
     }
     
     public int calculateCustomerFees(Customer customer){
         int price = 0;
         price = customer.getMembership().getPrice();
+        price = (price / customer.getMembership().getDurationInDays()) * 30;
         if(customer.getMembership().getHasPersonalTraining()){
             price += 50;
         }
